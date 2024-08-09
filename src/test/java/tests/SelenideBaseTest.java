8b78acc98f1226218d9120fae4a15da0cf255a11
a-levitt <artemis.levitt@gmail.com>
2024-08-09 19:01:13 +0200

    Basic selenide test on element visibility

--- src/test/java/tests/SelenideBaseTest.java
+++ src/test/java/tests/SelenideBaseTest.java
@@ -1,11 +1,20 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideBaseTest {
 
     @Test
-    public void test(){
+    public void test()  throws InterruptedException{
+        open("https://www.bankofcyprus.com/en-gb");
+        $x("//button[@id='ccc-recommended-settings']").click();
+        $x("//button[@class='trigger-search trigger-search-desktop']").click();
+        $x("//input[@id='searchQueryDesktop']").setValue("Loans").pressEnter();
+        $x("//div[@class='wrapper intro']/p").shouldBe(Condition.visible);
 
+        //Thread.sleep(10000);
     }
 }
